6be42f9865090d48b2a3896e13a197f53779cbbe
spencer <spencercs.ai@gmail.com>
2019-11-12 22:50:43 +0800

    Variable

--- src/MathCalc.java
+++ src/MathCalc.java
@@ -1,2 +1,13 @@
 public class MathCalc {
+    public static void main(String[] args){
+        System.out.println(5+6);
+        System.out.println(5-6);
+        System.out.println(5*6);
+        System.out.println(5/6.0);
+
+        System.out.println(1+2-3*4/5.0);
+
+        //引出变量的概念
+
+    }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,2 +1,16 @@
 public class Variable {
+    public static void main(String[] args) {
+        int a = 3; // int用来表示一个整数，取值范围在 -2^31~2^31-1
+        int b = 5;
+        int c = 7;
+
+        int x = 9; // 3，5，7，9 为字面值，a,b,c,x,y为变量
+
+        int  y = a*x + b*x*x + c*x*x*x;
+        /**
+         * 关键字：public,class,static,void, int
+         */
+
+        System.out.println(y);
+    }
 }
